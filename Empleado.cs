2d2e7650bb9f628d734ef30605f16c252c9fb29a
Rene Gonzalez <rene_glez1992@hotmail.com>
2021-04-02 16:46:43 -0700

    se eliminan usign que no se utilizaban y se agrega diccionario de datos

--- Empleado.cs
+++ Empleado.cs
@@ -1,8 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿/// <summary>
+/// Lenguaje de programacion III
+/// Autor: Rene Gonzalez Rodriguez
+/// Maestro: Aarón I. Salazar
+/// </summary>
 
 namespace Actividad_Integradora_3_Problema_2
 {
